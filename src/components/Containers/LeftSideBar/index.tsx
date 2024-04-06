b1968baf09a27d2faea16b7836b5f6d4ffb0df16
Sijan Mainali <sijanmainali2@gmail.com>
2024-04-06 01:02:16 -0500

    okkkkkkkk

--- src/components/Containers/LeftSideBar/index.tsx
+++ src/components/Containers/LeftSideBar/index.tsx
@@ -1,30 +1,62 @@
+'use client';
 import React from 'react';
 import style from './index.module.css';
 import Link from 'next/link';
 import {
+  HomeIconOutline,
   HomeIconSolid,
   LibraryIconSolid,
   PlusIcon,
   RightArrowIcon,
   SearchIconOutline,
+  SearchIconSolid,
 } from '@/components/Icons';
+import { usePathname } from 'next/navigation';
 
 function NavBar() {
+  const path = usePathname();
   return (
     <nav className={style.container}>
       {/* home and search */}
       <div className={style.home}>
         <ul className={style.ul}>
           <li className={style.li}>
-            <Link className={style.link} href={'/home'}>
-              <HomeIconSolid width={24} height={24} fill='#fff' />
-              <span className={style.span}>Home</span>
+            <Link className={style.link} href={'/'}>
+              {path === '/' ? (
+                <>
+                  <HomeIconSolid width={24} height={24} fill='#fff' />
+                  <span className={`text-white ${style.span}`}>Home</span>
+                </>
+              ) : (
+                <>
+                  <HomeIconOutline
+                    className='hovericon fill-[#b3b3b3]'
+                    width={24}
+                    height={24}
+                  />
+                  <span className={style.span}>Home</span>
+                </>
+              )}
             </Link>
           </li>
           <li className={style.li}>
-            <Link className={style.link} href={'/search'}>
-              <SearchIconOutline width={24} height={24} fill='#b3b3b3' />
-              <span className={style.span}>Search</span>
+            <Link className={`${style.link} group`} href={'/search'}>
+              {path === '/search' ? (
+                <>
+                  <SearchIconSolid width={24} height={24} fill='#fff' />
+                  <span className={`text-white ${style.span}`}>Search</span>
+                </>
+              ) : (
+                <>
+                  <SearchIconOutline
+                    className='hovericon'
+                    width={24}
+                    height={24}
+                    fill='#b3b3b3'
+                  />
+                  <span className={style.span}>Search</span>
+                </>
+              )}
             </Link>
           </li>
         </ul>
